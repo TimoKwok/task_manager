880b0e09632c386ec44425c16c0797c8ffc1e1e9
Timo Kwok <timothykwok17@gmail.com>
2024-09-28 17:25:48 -0400

    small change to icons

--- mix.exs
+++ mix.exs
@@ -58,7 +58,9 @@ defmodule TaskMaster.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:oban, "~> 2.17"}
+
     ]
   end
 
